4322df33718293e8e5a738ad51fcd57b37bd44e4
Vergissmeinnicht <85434293+Vergissmeinncht@users.noreply.github.com>
2025-11-20 16:43:51 +0800

    update qtran
    update qtran

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,8 +1,10 @@
 from .dmaq_qatten_learner import DMAQ_qattenLearner
 from .nq_learner import NQLearner
 from .nq_learner_data_augmentation import NQLearnerDataAugmentation
+from .qtran_learner import QLearner as QTranLearner
 REGISTRY = {}
 
+REGISTRY["qtran_learner"] = QTranLearner
 REGISTRY["nq_learner"] = NQLearner
 REGISTRY["dmaq_qatten_learner"] = DMAQ_qattenLearner
 REGISTRY["q_learner_data_augmentation"] = NQLearnerDataAugmentation

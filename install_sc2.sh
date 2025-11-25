2ace7cb1dee0b49e7e3945cb2c9c3152cdd8f135
Vergissmeinnicht <85434293+Vergissmeinncht@users.noreply.github.com>
2025-11-25 16:23:51 +0800

    Update install_sc2.sh

--- install_sc2.sh
+++ install_sc2.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 # Install SC2 and add the custom maps
 
-# Clone the source code.
-#git clone git@github.com:tjuHaoXiaotian/pymarl3.git
-export PYMARL3_CODE_DIR=$(pwd)
+if [ -z "$EXP_DIR" ]
+then
+    EXP_DIR=~
+fi
 
-# 1. Install StarCraftII
-echo 'Install StarCraftII...'
-cd "$HOME"
-export SC2PATH="$HOME/DP-QMix/3rdparty/StarCraftII"
+echo "EXP_DIR: $EXP_DIR"
+cd $EXP_DIR/pymarl
+
+mkdir 3rdparty
+cd 3rdparty
+
+export SC2PATH=`pwd`'/StarCraftII'
 echo 'SC2PATH is set to '$SC2PATH
+
 if [ ! -d $SC2PATH ]; then
         echo 'StarCraftII is not installed. Installing now ...';
-        wget http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.10.zip
-        unzip -P iagreetotheeula SC2.4.10.zip
+        wget http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.6.2.69232.zip
+        unzip -P iagreetotheeula SC2.4.6.2.69232.zip
+        rm -rf SC2.4.6.2.69232.zip
 else
         echo 'StarCraftII is already installed.'
 fi
 
-# 2. Install the custom maps
-
-# Copy the maps to the target dir.
-echo 'Install SMACV1 and SMACV2 maps...'
+echo 'Adding SMAC maps.'
 MAP_DIR="$SC2PATH/Maps/"
-if [ ! -d "$MAP_DIR/SMAC_Maps" ]; then
-    echo 'MAP_DIR is set to '$MAP_DIR
-    if [ ! -d $MAP_DIR ]; then
-            mkdir -p $MAP_DIR
-    fi
-    cp -r "$PYMARL3_CODE_DIR/src/envs/smac_v2/official/maps/SMAC_Maps" $MAP_DIR
-else
-    echo 'SMACV1 and SMACV2 maps are already installed.'
+echo 'MAP_DIR is set to '$MAP_DIR
+
+if [ ! -d $MAP_DIR ]; then
+        mkdir -p $MAP_DIR
 fi
-echo 'StarCraft II and SMAC maps are installed.'
+
+cd ..
+wget https://github.com/oxwhirl/smac/releases/download/v0.1-beta1/SMAC_Maps.zip
+unzip SMAC_Maps.zip
+mv SMAC_Maps $MAP_DIR
+rm -rf SMAC_Maps.zip
+
+echo 'StarCraft II and SMAC are installed.'
+
